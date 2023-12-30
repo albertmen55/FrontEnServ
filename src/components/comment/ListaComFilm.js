23f9d32148a65ed05d3ffab9a1d1883352826bd0
albertoparga <72313285+albertoparga@users.noreply.github.com>
2023-12-30 18:36:48 +0100

    Comentarios de películas

--- src/components/comment/ListaComFilm.js
+++ src/components/comment/ListaComFilm.js
@@ -1,4 +1,6 @@
 import { useUser } from "../../hooks"
+import ER from "./icons/e_rojo.png";
+import EB from "./icons/e_blanco.png";
 
 const picture = user => {
     if(user.content){
@@ -10,37 +12,43 @@ const picture = user => {
     }
 }
 
-export function ListaComFilm({comments = [], movie}) {
-    return <div className="overflow-x-scroll">
-        {comments.length === 0 ? (<span>No hay comentarios para esta pelicula.</span>) :
-            (<div className="min-w-min" /* style={{ width: "180rem" }} */>
-                <div className = 'p-5 flex flex-nowrap  space-x-12'  style={{ height: '26rem' }} >
+export function ListaComFilm({comments = []}) {
+    const { content = [] } = comments
+
+    return <div className="">
+        {content.length === 0 ? <h1>No se han encontrado comentarios</h1> :
+            <div className="min-w-min">
+                <div className = 'p-5 flex flex-col space-y-16'  >
                     {
-                        comments?.map((comment) =>
-                            <Element key = { comment.id } comment = {comment} movie = { movie }/>
+                        content?.map((comment, idx) =>
+                            <Element key = { comment.id } comment = {comment} index = { idx } total = { content.length }/>
                         )
                     }
                 </div>
-            </div>)}
+            </div>}
     </div>
 }
 
-function Element({comment, movie}) {
-    const {user} = useUser(comment.user.email)
-    return <>{movie?.id === undefined ? (<div></div>) :
-        (<div className = 'h-full min-w-min flex flex-col space-y-2 p-5 border rounded-md' style = {{boxShadow: '3px 5px rgb(0 0 0 / 0.15)', width: "55rem"}}>
-            <div className="flex justify-center items-center" >
-                    {movie?.id === comment?.movie.id ? <img style = {{ height: "25px" }}
-                                                          src = { picture(user) }
-                                                          alt = { `${ user?.email } poster` }
-                                                          className = 'rounded-full z-20' /> : null}
-                    <div className="flex-1 font-bold ml-2">
-                        <span className="">{user.email}</span>
-                    </div>
-                <div className="grow">
-                    <span className="">Puntuación: {comment?.rating}</span>
+function Element({comment, className = '', active, index, total}) {
+    const puntuacion = [1,2,3,4,5,6,7,8,9,10]
+
+    return <div className = 'min-w-min flex flex-col space-y-8 p-5 border rounded-md' style = {{boxShadow: '3px 5px rgb(0 0 0 / 0.15)'}}>
+        <div className="flex justify-center" >
+            <div className="flex-1 font-bold">
+                <span className="">{comment.user.email}</span>
+            </div>
+            <div className="puntuacion">
+                <div className="flex" style={{ height: '25px' }}>
+                    {puntuacion.map((estrella, idx) =>
+                        <img src = {idx+1 <= comment.rating ? ER : EB}
+                             id = {idx+1}
+                             alt = { `estrella ${idx}` }
+                             key = {idx}
+                             style = {{ width: '25px' }}/>
+                    )}
                 </div>
             </div>
-            <div className="font-serif">{comment?.comment}</div>
-        </div>)}</>
+        </div>
+        <div className="font-serif">{comment.comment}</div>
+    </div>
 }
